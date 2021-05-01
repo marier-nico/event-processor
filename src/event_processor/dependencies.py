5d54b4d37a0bf8e9bec12fa82c620ea6221c7101
Nicolas Marier <software@nmarier.com>
2021-05-01 13:21:39 -0400

    Finalize the dependency injection system

--- src/event_processor/dependencies.py
+++ src/event_processor/dependencies.py
@@ -1,8 +1,17 @@
-from typing import Callable, Any, Optional, List
+"""Dependency injection and management facilities."""
+import inspect
+from typing import Callable, Any, Optional, Dict, Tuple
 
 
 class Depends:
+    """Class to designate a dependency"""
+
     def __init__(self, callable_: Callable, cache: bool = True):
+        """Create a dependency.
+
+        :param callable_: The callable on which there is a dependency
+        :param cache: Whether or not to cache the value resulting from this dependency
+        """
         self.callable = callable_
         self.cache = cache
 
@@ -19,16 +28,64 @@ class Depends:
         return hash((self.callable, self.cache))
 
 
-def resolve(dependency: Depends, cache: Optional[dict] = None) -> Optional[Any]:
+def call_with_injection(callable_: Callable, cache: Optional[dict] = None, *args, **kwargs) -> Optional[Any]:
+    """Call a callable and inject required dependencies.
+
+    Note that keyword args that have the same name as the parameter used for a dependency will be overwritten with the
+    dependency's injected value.
+
+    :param callable_: The callable to call
+    :param cache: The dependency cache to use
+    :param args: The args to pass to the callable
+    :param kwargs: The kwargs to pass to the callable
+    :return: The return value of the callable
+    """
+    dependencies = get_required_dependencies(callable_)
+    for arg_name, dependency in dependencies.items():
+        kwargs[arg_name], cacheable = resolve(dependency, cache)
+
+    return callable_(*args, **kwargs)
+
+
+def resolve(dependency: Depends, cache: Optional[dict] = None) -> Tuple[Optional[Any], bool]:
+    """Resolve a dependency into a value.
+
+    The resulting values from dependencies are cached and re-used if a cache is supplied and the dependency itself
+    does not explicitly state that it does not want to be cached. Also, any dependency that depends on another
+    dependency where caching has been disabled will also not be cached (because the sub-value may change, which may in
+    turn change the value of the current dependency).
+
+    :param dependency: The dependency to resolve
+    :param cache: The cache for previously resolved dependencies
+    :return: The tuple (resolved_value, cacheable)
+    """
     if cache and dependency in cache:
         return cache[dependency]
 
-    value = dependency.callable()
-    if cache is not None and dependency.cache:
+    cacheable = dependency.cache
+    required_dependencies = get_required_dependencies(dependency.callable)
+    for arg_name, required_dependency in required_dependencies.items():
+        required_dependencies[arg_name], cacheable_dep = resolve(required_dependency, cache)
+        cacheable = cacheable and cacheable_dep
+
+    value = dependency.callable(**required_dependencies)
+
+    if cache is not None and cacheable:
         cache[dependency] = value
 
-    return value
+    return value, cacheable
 
 
-def get_required_dependencies(callable_: Callable) -> List[Depends]:
-    return []
+def get_required_dependencies(callable_: Callable) -> Dict[str, Depends]:
+    """Get the required dependencies for a callable.
+
+    :param callable_: The callable for which to get dependencies
+    :return: A mapping of callable argument names to dependencies
+    """
+    signature = inspect.signature(callable_)
+    required_dependencies = {
+        name: arg.default
+        for name, arg in signature.parameters.items()
+        if arg.default is not inspect.Parameter.empty and isinstance(arg.default, Depends)
+    }
+    return required_dependencies
--- src/tests/test_dependencies.py
+++ src/tests/test_dependencies.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from event_processor.dependencies import Depends, resolve, get_required_dependencies
+from event_processor.dependencies import Depends, resolve, get_required_dependencies, call_with_injection
 
 
 def test_depends_init_sets_values():
@@ -31,6 +31,41 @@ def test_depends_eq_compares_correctly():
     assert depends_fn != "not a depends"
 
 
+def test_call_with_injection_calls_callable():
+    callable_mock = Mock()
+
+    call_with_injection(callable_mock)
+
+    callable_mock.assert_called_once()
+
+
+def test_call_with_injection_passes_parameters():
+    callable_mock = Mock()
+
+    call_with_injection(callable_mock, None, 1, 2, x=3)
+
+    callable_mock.assert_called_once_with(1, 2, x=3)
+
+
+def test_call_with_injection_injects_dependencies():
+    dependency = Mock()
+
+    def fn(a: Mock = Depends(dependency)):
+        a.method()
+
+    call_with_injection(fn)
+
+    dependency.return_value.method.assert_called_once()
+
+
+def test_call_with_injection_returns_callable_value():
+    callable_mock = Mock()
+
+    result = call_with_injection(callable_mock)
+
+    assert result is callable_mock.return_value
+
+
 def test_resolve_calls_callable():
     mock_callable = Mock()
     dependency = Depends(mock_callable)
@@ -44,7 +79,7 @@ def test_resolve_returns_callable_result():
     mock_callable = Mock()
     dependency = Depends(mock_callable)
 
-    result = resolve(dependency)
+    result, _ = resolve(dependency)
 
     assert result is mock_callable.return_value
 
@@ -71,21 +106,57 @@ def test_resolve_does_not_use_cache_with_caching_disabled():
     assert len(mock_callable.mock_calls) == 2
 
 
+def test_resolve_injects_required_dependencies_for_nested_dependencies():
+    mock_callable = Mock()
+    dependency = Depends(mock_callable, cache=False)
+
+    def fn(a: Mock = dependency):
+        return a
+
+    nested_dependency = Depends(fn)
+    result, _ = resolve(nested_dependency)
+
+    assert result is mock_callable.return_value
+
+
+def test_resolve_only_caches_values_that_require_other_cached_values():
+    mock_callable = Mock()
+    cached_dependency = Depends(mock_callable, cache=True)
+
+    def fn(_=cached_dependency):
+        pass
+
+    no_cache_dependency = Depends(fn, cache=False)
+
+    def no_cache_fn(_=no_cache_dependency):
+        pass
+
+    top_level_caching = Depends(no_cache_fn, cache=True)
+
+    cache = {}
+    resolve(top_level_caching, cache)
+
+    assert cached_dependency in cache
+    assert no_cache_dependency not in cache
+    assert top_level_caching not in cache  # Because it depends on a value that shouldn't be cached
+
+
 def test_get_required_dependencies_returns_empty_list_for_no_dependencies():
     def fn():
         pass
 
     dependencies = get_required_dependencies(fn)
 
-    assert dependencies == []
+    assert dependencies == {}
 
 
 def test_get_required_dependencies_returns_all_specified_dependencies():
     dependency = Depends(lambda: 0)
+    other_dependency = Depends(lambda: 1)
 
-    def fn(a=dependency):
+    def fn(_a=dependency, _b=other_dependency):
         pass
 
     dependencies = get_required_dependencies(fn)
 
-    assert dependencies == [dependency]
+    assert dependencies == {"_a": dependency, "_b": other_dependency}
