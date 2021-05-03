09f0832fab94e83b11887e1f27aa2af4705dab08
Nicolas Marier <software@nmarier.com>
2021-05-02 20:10:16 -0400

    Add calculating the specificity of a filter

--- src/event_processor/filters.py
+++ src/event_processor/filters.py
@@ -15,6 +15,17 @@ class Filter(ABC):
         """
 
     @abstractmethod
+    def get_match_specificity(self, event: dict) -> int:
+        """Calculate the specificity of a filter match.
+
+        This represents (roughly) how many filters and sub-filters matched on a given event. This is used to determine
+        which processor should be invoked for a given event.
+
+        :param event: The event to evaluate
+        :return: The filter's match specificity
+        """
+
+    @abstractmethod
     def __hash__(self):
         """Hash a filter for storage in a dict or set."""
 
@@ -58,6 +69,9 @@ class Accept(Filter):
     def matches(self, _event: dict) -> bool:
         return True
 
+    def get_match_specificity(self, _event: dict) -> int:
+        return 1
+
     def __hash__(self):
         return hash(self.__class__)
 
@@ -80,6 +94,9 @@ class Exists(Filter):
                 return False
 
         return True
+
+    def get_match_specificity(self, event: dict) -> int:
+        return 1 if self.matches(event) else 0
 
     def __hash__(self):
         return hash((self.__class__, self.path))
@@ -107,6 +124,9 @@ class Eq(Filter):
 
         return False
 
+    def get_match_specificity(self, event: dict) -> int:
+        return 1 if self.matches(event) else 0
+
     def __hash__(self):
         return hash((self.__class__, (self.path, self.value)))
 
@@ -124,6 +144,9 @@ class And(Filter):
 
     def matches(self, event: dict) -> bool:
         return all(filter_.matches(event) for filter_ in self.filters)
+
+    def get_match_specificity(self, event: dict) -> int:
+        return sum(filter_.get_match_specificity(event) for filter_ in self.filters) + 2
 
     def __hash__(self):
         return hash((self.__class__, self.filters))
@@ -144,6 +167,9 @@ class Or(Filter):
 
     def matches(self, event: dict) -> bool:
         return any(filter_.matches(event) for filter_ in self.filters)
+
+    def get_match_specificity(self, event: dict) -> int:
+        return sum(filter_.get_match_specificity(event) for filter_ in self.filters) + 1
 
     def __hash__(self):
         return hash((self.__class__, self.filters))
