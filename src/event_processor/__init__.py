bd11a712c37cceba1609b591f3619c2ab99e0ed7
Nicolas Marier <software@nmarier.com>
2021-05-02 21:37:27 -0400

    Update exports

--- src/event_processor/__init__.py
+++ src/event_processor/__init__.py
@@ -1,4 +1,6 @@
-from .event_processor import EventProcessor  # noqa
+from . import filters  # noqa
+from .dependencies import Depends  # noqa
+from .event_processor import EventProcessor, Event  # noqa
 from .exceptions import (  # noqa
     EventProcessorError,
     FilterError,
